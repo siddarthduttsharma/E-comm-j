176b92d5d3faf6cade7442b86de660d2b88fa785
Siddarth Dutt <siddarthduttsharma@gmail.com>
2024-04-17 13:16:18 +0530

    Initial commit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,8 +6,10 @@ import ProductCard from "../components/product-card";
 import { useLatestProductsQuery } from "../redux/api/productAPI";
 import { addToCart } from "../redux/reducer/cartReducer";
 import { CartItem } from "../types/types";
+import downloadImage from "../assets/download.jpg";
 
 const Home = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, isLoading, isError } = useLatestProductsQuery("");
 
   const dispatch = useDispatch();
@@ -32,21 +34,59 @@ const Home = () => {
       </h1>
 
       <main>
-        {isLoading ? (
-          <Skeleton width="80vw" />
-        ) : (
-          data?.products.map((i) => (
-            <ProductCard
-              key={i._id}
-              productId={i._id}
-              name={i.name}
-              price={i.price}
-              stock={i.stock}
-              handler={addToCartHandler}
-              photo={i.photo}
-            />
-          ))
-        )}
+      {isLoading ? (
+  <Skeleton width="80vw" />
+) : (
+  // Displaying simple mock data for ProductCard components
+  <>
+    <ProductCard
+      key="1"
+      productId="1"
+      name="Product 1"
+      price={10}
+      stock={10}
+      handler={addToCartHandler}
+      photo="https://www.google.com/imgres?q=product%20images%20jpg&imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F2536965%2Fpexels-photo-2536965.jpeg&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fproduct%2F&docid=nMjIODhQZ_AKUM&tbnid=bHd09wELR0lfwM&vet=12ahUKEwi9zvDsiaSFAxUxcvUHHdWjDl0QM3oECFcQAA..i&w=3743&h=3040&hcb=2&ved=2ahUKEwi9zvDsiaSFAxUxcvUHHdWjDl0QM3oECFcQAA"
+    />
+    <ProductCard
+      key="2"
+      productId="2"
+      name="Product 2"
+      price={10}
+      stock={5}
+      handler={addToCartHandler}
+      photo={downloadImage}
+    />
+    <ProductCard
+      key="3"
+      productId="3"
+      name="Product 3"
+      price={10}
+      stock={15}
+      handler={addToCartHandler}
+      photo={downloadImage}
+    />
+    <ProductCard
+      key="3"
+      productId="3"
+      name="Product 3"
+      price={10}
+      stock={15}
+      handler={addToCartHandler}
+      photo={downloadImage}
+    />
+    <ProductCard
+      key="3"
+      productId="3"
+      name="Product 3"
+      price={10}
+      stock={15}
+      handler={addToCartHandler}
+      photo={downloadImage}
+    />
+  </>
+)}
+
       </main>
     </div>
   );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,99 @@
+// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+// import { FcGoogle } from "react-icons/fc";
+// import { auth } from "../firebase";
+// import { getUser, useLoginMutation } from "../redux/api/userAPI";
+// import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+// import { MessageResponse } from "../types/api-types";
+// import { userExist, userNotExist } from "../redux/reducer/userReducer";
+// import { useDispatch } from "react-redux";
+
+// const Login = () => {
+//   const dispatch = useDispatch();
+//   const [gender, setGender] = useState("");
+//   const [date, setDate] = useState("");
+
+//   const [login] = useLoginMutation();
+
+//   const loginHandler = async () => {
+//     try {
+//       const provider = new GoogleAuthProvider();
+//       const { user } = await signInWithPopup(auth, provider);
+
+//       console.log({
+//         name: user.displayName!,
+//         email: user.email!,
+//         photo: user.photoURL!,
+//         gender,
+//         role: "user",
+//         dob: date,
+//         _id: user.uid,
+//       });
+
+//       const res = await login({
+//         name: user.displayName!,
+//         email: user.email!,
+//         photo: user.photoURL!,
+//         gender,
+//         role: "user",
+//         dob: date,
+//         _id: user.uid,
+//       });
+
+//       if ("data" in res) {
+//         toast.success(res.data.message);
+//         const data = await getUser(user.uid);
+//         // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+//         dispatch(userExist(data?.user!));
+//       } else {
+//         const error = res.error as FetchBaseQueryError;
+//         const message = (error.data as MessageResponse).message;
+//         toast.error(message);
+//         dispatch(userNotExist());
+//       }
+//     } catch (error) {
+//       toast.error("Sign In Fail");
+//     }
+//   };
+
+//   return (
+//     <div className="login">
+//       <main>
+//         <h1 className="heading">Login</h1>
+
+//         <div>
+//           <label>Gender</label>
+//           <select value={gender} onChange={(e) => setGender(e.target.value)}>
+//             <option value="">Select Gender</option>
+//             <option value="male">Male</option>
+//             <option value="female">Female</option>
+//           </select>
+//         </div>
+
+//         <div>
+//           <label>Date of birth</label>
+//           <input
+//             type="date"
+//             value={date}
+//             onChange={(e) => setDate(e.target.value)}
+//           />
+//         </div>
+
+//         <div>
+//           <p>Already Signed In Once</p>
+//           <button onClick={loginHandler}>
+//             <FcGoogle /> <span>Sign in with Google</span>
+//           </button>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -11,8 +107,8 @@ import { useDispatch } from "react-redux";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const [gender, setGender] = useState("");
-  const [date, setDate] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [login] = useLoginMutation();
 
@@ -21,29 +117,20 @@ const Login = () => {
       const provider = new GoogleAuthProvider();
       const { user } = await signInWithPopup(auth, provider);
 
-      console.log({
-        name: user.displayName!,
-        email: user.email!,
-        photo: user.photoURL!,
-        gender,
-        role: "user",
-        dob: date,
-        _id: user.uid,
-      });
-
       const res = await login({
         name: user.displayName!,
-        email: user.email!,
+        email: email,
         photo: user.photoURL!,
-        gender,
+        gender: "", // No longer needed
         role: "user",
-        dob: date,
+        dob: password,
         _id: user.uid,
       });
 
       if ("data" in res) {
         toast.success(res.data.message);
         const data = await getUser(user.uid);
+        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
         dispatch(userExist(data?.user!));
       } else {
         const error = res.error as FetchBaseQueryError;
@@ -62,21 +149,25 @@ const Login = () => {
         <h1 className="heading">Login</h1>
 
         <div>
-          <label>Gender</label>
-          <select value={gender} onChange={(e) => setGender(e.target.value)}>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div>
-          <label>Date of birth</label>
+          <label>Password</label>
           <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+        </div>
+
+        <div>
+          <button className="btn btn-primary logbutton">Login</button>
         </div>
 
         <div>
